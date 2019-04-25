53b4c24799f404126d6811a92f18f16a54a45b71
sugarless <quangnd@foobla.com>
2019-04-25 22:33:16 +0700

    ssh

--- robo.py
+++ robo.py
@@ -9,29 +9,55 @@ controller.start()
 # Bedrooms: FloorPlan301 - FloorPlan330
 # Bathrooms: FloorPLan401 - FloorPlan430
 
-controller.step(dict(action='Initialize', gridSize=0.25, rotation=1))
+controller.reset('FloorPlan28')
+event = controller.step(dict(action='Initialize', gridSize=0.05, rotation=-0.01))
+
+def getInfo(_event):
+    for o in _event.metadata['objects']:
+        if o['visible'] and o['receptacle']:
+            receptacle_object_id = o['objectId']
+            return receptacle_object_id
 
 while True:
     if   keyboard.is_pressed("d"):
-        controller.step(dict(action="MoveRight"))
-        time.sleep(0.5)
+        event = controller.step(dict(action="MoveRight"))
+        # print(event.metadata['objects'])
     elif keyboard.is_pressed("s"):
-        controller.step(dict(action="MoveBack"))
-        time.sleep(0.5)
+        event = controller.step(dict(action="MoveBack"))
     elif keyboard.is_pressed("a"):
-        controller.step(dict(action="MoveLeft"))
-        time.sleep(0.5)
+        event = controller.step(dict(action="MoveLeft"))
     elif keyboard.is_pressed("w"):
-        controller.step(dict(action="MoveAhead"))
+        event = controller.step(dict(action="MoveAhead"))
+    elif keyboard.is_pressed("j"):
+        event = controller.step(dict(action="RotateLeft"))
         time.sleep(0.5)
-    elif keyboard.is_pressed("q"):
-        controller.step(dict(action="RotateLeft"))
+    elif keyboard.is_pressed("l"):
+        event = controller.step(dict(action="RotateRight"))
         time.sleep(0.5)
-    elif keyboard.is_pressed("e"):
-        controller.step(dict(action="RotateRight"))
+    elif keyboard.is_pressed("i"):
+        event = controller.step(dict(action="LookUp"))
         time.sleep(0.5)
-    elif keyboard.is_pressed("2"):
-        controller.step(dict(action="LookUp"))
+    elif keyboard.is_pressed("k"):
+        event = controller.step(dict(action="LookDown"))
         time.sleep(0.5)
+    elif keyboard.is_pressed("p"):
+        dist = dict()
+        for o in event.metadata['objects']:
+            if o['visible'] and o['pickupable']:
+                dist[o['objectId']] = o['distance']
+        # print(dist)
+        minDist_objectId = min(dist, key=dist.get)
+        event = controller.step(dict(action='PickupObject', objectId=minDist_objectId), raise_for_failure=True)
+        object_id = minDist_objectId
+        time.sleep(1)
+    elif keyboard.is_pressed("["):
+        dist = dict()
+        for o in event.metadata['objects']:
+            if o['receptacle']:
+                dist[o['objectId']] = o['distance']
+        minDist_receptacleObjectId = min(dist, key=dist.get)
+        event = controller.step(dict(action='PutObject', receptacleObjectId=minDist_receptacleObjectId, objectId=object_id), raise_for_failure=True)
+        time.sleep(1)
+
     else:
         pass
---
+++ test.py
@@ -0,0 +1,4 @@
+dic = dict()
+dic['a'] = str(1)
+dic['b'] = str(2)
+min(dic, key=dic.get)
