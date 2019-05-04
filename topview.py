3f81b79a66a60e1330de21bc15905ec023d76a32
Minh Mon <minhmon@Minhs-iMac.local>
2019-05-04 08:56:49 +0700

    upload topview origin version

--- topview.py
+++ topview.py
@@ -1,9 +1,8 @@
 import math
-
 import matplotlib
+import matplotlib.pyplot as plt
 from ai2thor.controller import Controller
-
-#matplotlib.use("TKAgg", warn=False)
+from ai2thor.controller import BFSController
 
 from PIL import Image, ImageDraw
 
@@ -17,28 +16,12 @@ class ThorPositionTo2DFrameTranslator(object):
         self.lower_left = np.array((cam_position[0], cam_position[2])) - orth_size
         self.span = 2 * orth_size
 
-    def __call__(self, position):
-        if len(position) == 3:
-            x, _, z = position
-        else:
-            x, z = position
-
-        camera_position = (np.array((x, z)) - self.lower_left) / self.span
-        return np.array(
-            (
-                round(self.frame_shape[0] * (1.0 - camera_position[1])),
-                round(self.frame_shape[1] * camera_position[0]),
-            ),
-            dtype=int,
-        )
-
-
 def position_to_tuple(position):
     return (position["x"], position["y"], position["z"])
 
 
 def get_agent_map_data(c: Controller):
-    c.step({"action": "ToggleMapView"})
+    c.step(dict(action = 'ToggleMapView'))
     cam_position = c.last_event.metadata["cameraPosition"]
     cam_orth_size = c.last_event.metadata["cameraOrthSize"]
     pos_translator = ThorPositionTo2DFrameTranslator(
@@ -48,18 +31,15 @@ def get_agent_map_data(c: Controller):
         "frame": c.last_event.frame,
         "cam_position": cam_position,
         "cam_orth_size": cam_orth_size,
-        "pos_translator": pos_translator,
+        "pos_translator": pos_translator
     }
-    c.step({"action": "ToggleMapView"})
+    c.step(dict(action = 'ToggleMapView'))
     return to_return
 
-if __name__ == "__main__":
-    import matplotlib.pyplot as plt
-    
-    c = Controller()
-    c.start()
-    c.reset("FloorPlan308")
-    
-    t = get_agent_map_data(c)
-    plt.imshow(t["frame"])
-    plt.show()
+if __name__ == '__main__': 
+	c = Controller()
+	c.start()
+	c.reset("FloorPlan1")
+	topview = get_agent_map_data(c)
+	plt.imshow(topview["frame"])
+	plt.show()
