f0d52cdc95356221689d6034bc5795ca7ae4cc30
sugarless <quangnd@foobla.com>
2019-05-06 00:11:29 +0700

    .

--- object_detect.py
+++ object_detect.py
@@ -1,5 +1,2 @@
 from object_detection.yolo_opencv import object_detector as yolo
 
-
-img = 'object_detection/cat.jpg'
-yolo(img)
--- robo.py
+++ robo.py
@@ -1,6 +1,8 @@
 import ai2thor.controller
 import keyboard
 import time
+from object_detection.yolo_opencv import object_detector as yolo
+import cv2
 controller = ai2thor.controller.Controller()
 controller.start()
 
@@ -9,34 +11,37 @@ controller.start()
 # Bedrooms: FloorPlan301 - FloorPlan330
 # Bathrooms: FloorPLan401 - FloorPlan430
 
-controller.reset('FloorPlan18')
-event = controller.step(dict(action='Initialize', gridSize=0.05, rotation=-0.01))
+controller.reset('FloorPlan25')
+event = controller.step(dict(action='Initialize', gridSize=0.05, renderClassImage=True, renderObjectImage=True))
 
 while True:
     if   keyboard.is_pressed("d"):
         event = controller.step(dict(action="MoveRight"))
-        if event.metadata['lastActionSuccess'] is True:
-            print(1)
-        # print(event.metadata['objects'])
+        yolo(event.cv2img)
     elif keyboard.is_pressed("s"):
         event = controller.step(dict(action="MoveBack"))
+        yolo(event.cv2img)
     elif keyboard.is_pressed("a"):
         event = controller.step(dict(action="MoveLeft"))
+        yolo(event.cv2img)
     elif keyboard.is_pressed("w"):
         event = controller.step(dict(action="MoveAhead"))
+        yolo(event.cv2img)
     elif keyboard.is_pressed("j"):
         event = controller.step(dict(action="RotateLeft"))
+        yolo(event.cv2img)
         time.sleep(0.5)
     elif keyboard.is_pressed("l"):
         event = controller.step(dict(action="RotateRight"))
+        yolo(event.cv2img)
         time.sleep(0.5)
     elif keyboard.is_pressed("i"):
         event = controller.step(dict(action="LookUp"))
+        yolo(event.cv2img)
         time.sleep(0.5)
     elif keyboard.is_pressed("k"):
         event = controller.step(dict(action="LookDown"))
+        yolo(event.cv2img)
         time.sleep(0.5)
-    
-
     else:
         pass
